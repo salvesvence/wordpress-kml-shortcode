32c81fa4ce33b68459e6bcddbaf002489667dc6a
salvesvence <salvesvence@gmail.com>
2016-05-26 15:07:07 +0200

    Created the shortcode script file.

---
+++ kml_shortcode.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: WP Kml Shortcode
+Plugin URI: https://github.com/salvesvence/wordpress-kml-shortcode
+Description: Shortcode to load a kml map into your Wordpress Theme.
+Version: 1.0
+Author: Silvano Alves Vence
+Author URI: https://github.com/salvesvence
+*/
