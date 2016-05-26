61e605b548ece314466712d0b472047fa80bf14f
salvesvence <salvesvence@gmail.com>
2016-05-26 16:42:00 +0200

    Added the initial content into the kml_shortcode content.

--- kml_shortcode.php
+++ kml_shortcode.php
@@ -7,3 +7,23 @@ Version: 1.0
 Author: Silvano Alves Vence
 Author URI: https://github.com/salvesvence
 */
+
+add_shortcode('kml_map', function($attrs) {
+
+    $args = shortcode_atts(
+        array(
+            'name' => 'via-de-la-plata-74211',
+            'coordinates' => '42.811609,-7.5764603',
+            'zoom' => '8',
+            'width' => '100%',
+            'height' => '300px'
+        ), $attrs
+    );
+
+    extract($args);
+
+    $coords = explode( ',', $coordinates);
+    $zoom = (int) $zoom;
+
+    return "<div id='$name' class='kml-map' data-lat='$coords[0]' data-lng='$coords[1]' data-zoom='$zoom' data-width='$width' data-height='$height'></div>";
+});
