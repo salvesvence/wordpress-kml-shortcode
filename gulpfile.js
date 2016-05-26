dc7933a64c58b5a57a8da31bd704e70b29af65bd
salvesvence <salvesvence@gmail.com>
2016-05-26 16:57:37 +0200

    Added the node_modules directory into the .gitignore file.

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,20 @@
 var gulp = require('gulp'),
-    cssmin = require('gulp-cssmin'),
     rename = require('gulp-rename'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify');
-
-gulp.task('minify', function() {
-
-    gulp.src('wp-content/resources/css/app.css')
-        .pipe(cssmin())
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('wp-content/resources/css'));
-});
 
 gulp.task('uglify', function() {
 
     gulp.src(
         [
-            'wp-content/resources/js/ZipFile.complete.js',
-            'wp-content/resources/js/geoxml3.js',
-            'wp-content/resources/js/app.js'
+            'resources/js/ZipFile.complete.js',
+            'resources/js/geoxml3.js',
+            'resources/js/app.js'
         ])
         .pipe(concat('app.js'))
         .pipe(rename({suffix: '.min'}))
         .pipe(uglify())
-        .pipe(gulp.dest('wp-content/resources/js'));
+        .pipe(gulp.dest('resources/js'));
 });
 
 gulp.task('default', ['minify', 'uglify'], function(){});
